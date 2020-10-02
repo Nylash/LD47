4b91d1eef14335771a0d6dab9c2774fab7afb551
Kraghan <madjojo70@live.fr>
2020-10-03 00:24:45 +0200

    Add main menu

---
+++ LD47/Assets/Scripts/Utils/LevelHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelHandler : MonoBehaviour
+{
+    public void LoadLevel(int levelIndex)
+    {
+        SceneManager.LoadScene(levelIndex);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}

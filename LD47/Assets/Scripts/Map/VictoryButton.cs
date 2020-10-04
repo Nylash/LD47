2ba9a2a91eb65aa607a16322d9701890f05e6413
Ben <ben.peltier7@gmail.com>
2020-10-05 00:22:36 +0200

    Victory Button

---
+++ LD47/Assets/Scripts/Map/VictoryButton.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictoryButton : ButtonGameplay
+{
+    [HideInInspector]
+    [SerializeField] private GameObject VictoryModel = null;
+
+    public override void InteractEnter(Character player)
+    {
+        UI_Manager.instance.Victory();
+    }
+
+    public override void InteractExit(Character player)
+    {
+        return;
+    }
+
+    protected override void EditorStart()
+    {
+        ObjectRef = GetObjectRef();
+        InteractionLayer = -1;
+    }
+
+    protected override void EditorUpdate()
+    {
+        return;
+    }
+
+    protected override GameObject GetObjectRef()
+    {
+        if (!ObjectRef)
+        {
+            return Instantiate(VictoryModel, transform);
+        }
+        else
+        {
+            return ObjectRef;
+        }
+    }
+}
